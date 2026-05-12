6dfa0841b5e95aea519917fc38511953c2497180
melissamoe <melissa.moegen@viacesi.fr>
2026-05-12 15:31:58 +0200

    base de données

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -12,18 +12,28 @@ class Database
     public static function connect(): PDO
     {
         if (self::$pdo === null) {
+
+            $host = $_ENV['DB_HOST'];
+            $dbname = $_ENV['DB_NAME'];
+            $user = $_ENV['DB_USER'];
+            $password = $_ENV['DB_PASSWORD'];
+
             try {
+
                 self::$pdo = new PDO(
-                    'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8mb4',
-                    $_ENV['DB_USER'],
-                    $_ENV['DB_PASSWORD'],
+                    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+                    $user,
+                    $password,
                     [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     ]
                 );
+
             } catch (PDOException $e) {
+
                 die('Erreur de connexion à la base de données.');
+
             }
         }
 
--- public/index.php
+++ public/index.php
@@ -10,6 +10,12 @@ session_start();
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
+use App\Core\Database;
+
+Database::connect();
+echo "Connexion base de données OK";
+exit;
+
 $router = new Router();
 
 $router->get('/', [HomeController::class, 'index']);
